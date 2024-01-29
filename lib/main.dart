b77b06c5d2be4f8d495514673487561e6b2eb118
ngobiea <ngobiea@gmail.com>
2024-01-29 12:37:12 +0500

    complete List_view

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,161 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
+  var widgets = [
+    const ListTile(
+      leading: Text(
+        'J',
+        style: TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+      title: Text('John Rambo'),
+      subtitle: Text('Never runs out of bullets!'),
+      trailing: Text(
+        'Invincible',
+        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+      ),
+    ),
+    const ListTile(
+      leading: Text(
+        'P',
+        style: TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+      title: Text('Peter Pollock'),
+      subtitle: Text('Bowler'),
+      trailing: Text(
+        '50 wickets',
+        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+      ),
+    ),
+     const ListTile(
+      leading: Text(
+        'P',
+        style: TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+      title: Text('Peter Pollock'),
+      subtitle: Text('Bowler'),
+      trailing: Text(
+        '50 wickets',
+        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+      ),
+    ),
+     const ListTile(
+      leading: Text(
+        'P',
+        style: TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+      title: Text('Peter Pollock'),
+      subtitle: Text('Bowler'),
+      trailing: Text(
+        '50 wickets',
+        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+      ),
+    ),
+  ];
   runApp(
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.black,
+          backgroundColor: Colors.blue,
           title: const Text(
-            'Rows & Columns',
+            'ListView in Flutter',
             style: TextStyle(color: Colors.white),
           ),
         ),
-        body: Center(
-          child: Container(
-            color: Colors.black,
-            height: 300,
-            width: 300,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // crossAxisAlignment: CrossAxisAlignment.end,
-              // verticalDirection: VerticalDirection.up,
-              children: [
-                Container(
-                  width: 100,
-                  height: 40,
-                  color: Colors.green,
-                ),
-                Container(
-                  width: 100,
-                  height: 40,
-                  color: Colors.white,
-                ),
-                Container(
-                  width: 100,
-                  height: 40,
-                  color: Colors.blue,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  textBaseline: TextBaseline.alphabetic,
-                  // textDirection: TextDirection.rtl,
-                  children: [
-                    Text(
-                      'Le',
-                      style: TextStyle(
-                        color: Colors.green,
-                        fontSize: 15,
-                      ),
-                    ),
-                    Text(
-                      '.................',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      'Le3000',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 30,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+        body: Column(
+          children: [
+            SizedBox(
+              // color: Colors.black,
+              height: 200,
+
+              child: ListView.builder(
+                  itemCount: widgets.length,
+                  itemBuilder: (context, index) {
+                    return widgets[index];
+                  }),
+              // child: ListView(
+              //   scrollDirection: Axis.horizontal,
+              //   padding: const EdgeInsets.all(8),
+              //   children: const [
+              //     Text(
+              //       'Peter',
+              //       style: TextStyle(fontSize: 20, color: Colors.blue),
+              //     ),
+              //     SizedBox(
+              //       width: 20,
+              //     ),
+              //     Text(
+              //       'Paul',
+              //       style: TextStyle(fontSize: 20, color: Colors.blue),
+              //     ),
+              //     SizedBox(
+              //       width: 20,
+              //     ),
+              //     Text(
+              //       'John',
+              //       style: TextStyle(fontSize: 20, color: Colors.blue),
+              //     ),
+              //     SizedBox(
+              //       width: 20,
+              //     ),
+              //     Text(
+              //       'Peter',
+              //       style: TextStyle(fontSize: 20, color: Colors.blue),
+              //     ),
+              //     SizedBox(
+              //       width: 20,
+              //     ),
+              //     Text(
+              //       'Paul',
+              //       style: TextStyle(fontSize: 20, color: Colors.blue),
+              //     ),
+              //     SizedBox(
+              //       width: 20,
+              //     ),
+              //     Text(
+              //       'John',
+              //       style: TextStyle(fontSize: 20, color: Colors.blue),
+              //     ),
+              //         SizedBox(
+              //       width: 20,
+              //     ),
+              //     Text(
+              //       'Peter',
+              //       style: TextStyle(fontSize: 20, color: Colors.blue),
+              //     ),
+              //     SizedBox(
+              //       width: 20,
+              //     ),
+              //     Text(
+              //       'Paul',
+              //       style: TextStyle(fontSize: 20, color: Colors.blue),
+              //     ),
+              //     SizedBox(
+              //       width: 20,
+              //     ),
+              //     Text(
+              //       'John',
+              //       style: TextStyle(fontSize: 20, color: Colors.blue),
+              //     ),
+              //   ],
+              // ),
             ),
-          ),
+            const Text(
+              'Hello World',
+              style: TextStyle(fontSize: 40, color: Colors.blue),
+            )
+          ],
         ),
       ),
     ),
