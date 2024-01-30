7bbbd5dc814a2e06f3905b0bc0d96f92cca33253
ngobiea <ngobiea@gmail.com>
2024-01-30 16:27:30 +0500

    complete stack and positioning

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Stack and Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
@@ -32,92 +31,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {
-    setState(() {});
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: ListView(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/profile.jpg'),
-                  radius: 70,
-                )
-              ],
-            ),
-          ),
-          Card(
-            elevation: 20,
-            margin: const EdgeInsets.all(8),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(
-                20.0,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              top: 50,
+              left: 50,
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.red,
               ),
             ),
-            clipBehavior: Clip.antiAlias,
-            child: Image.network(
-              'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1xw:0.74975xh;center,top&resize=1200:*',
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) {
-                  return child;
-                }
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              },
-            ),
-          ),
-          Image.network(
-            'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1xw:0.74975xh;center,top&resize=1200:*',
-            loadingBuilder: (context, child, loadingProgress) {
-              if (loadingProgress == null) {
-                return child;
-              }
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            },
-          ),
-          CachedNetworkImage(
-            imageUrl:
-                'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1xw:0.74975xh;center,top&resize=1200:*',
-            placeholder: (context, url) {
-              return const Image(
-                image: AssetImage('assets/images/placeholder.png'),
-              );
-            },
-            errorWidget: (context, url, error) => const Icon(
-              Icons.error,
-              size: 100,
-              color: Colors.black,
-            ),
-          ),
-          Image.asset(
-            'assets/images/profile.jpg',
-          ),
-          const Image(
-            image: AssetImage(
-              'assets/images/profile.jpg',
-            ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+            const Positioned(
+              top: 100,
+              left: 50,
+              child: Icon(
+                Icons.check,
+                color: Colors.white,
+                size: 50,
+              ),
+            )
+          ],
+        ));
   }
 }
