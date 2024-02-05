5db71d5ca15ce7b5b7bed9c0d35708e488b1be7a
ngobiea <ngobiea@gmail.com>
2024-02-05 09:45:00 +0500

    complete side navigation_drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/defaults/defaults.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,131 +11,247 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Stateless and Stateful",
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          backgroundColor: Colors.blue,
-        ),
-        body: const Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Welcome to the app',
-                style: TextStyle(
-                  fontSize: 30,
-                ),
-              ),
-              SizedBox(height: 20),
-              Buttons(text: 'Hello'),
-              SizedBox(height: 20),
-              FooterWidget()
-            ],
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: MainPage(),
     );
   }
 }
 
-class Buttons extends StatefulWidget {
-  const Buttons({super.key, required this.text});
-  final String text;
+var indexClicked = 1;
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
 
   @override
-  State<Buttons> createState() => _ButtonsState();
+  State<MainPage> createState() => _MainPageState();
 }
 
-class _ButtonsState extends State<Buttons> {
-  var maleCounter = 0;
-  var femaleCounter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      Text(
-        'Nr of Males: $maleCounter',
-        style: const TextStyle(
-          fontSize: 20,
-        ),
-      ),
-      Text(
-        'Nr of Females: $femaleCounter',
-        style: const TextStyle(
-          fontSize: 20,
-        ),
-      ),
-      const SizedBox(height: 20),
-      MyOwnButton(
-          onTap: () {
-            setState(() {
-              maleCounter++;
-              print(widget.text);
-            });
-          },
-          text: 'Male +'),
-      const SizedBox(height: 20),
-      MyOwnButton(
-          onTap: () {
-            setState(() {
-              femaleCounter++;
-              print(widget.text);
-            });
-          },
-          text: 'Female +'),
-    ]);
-  }
-}
-
-class MyOwnButton extends StatelessWidget {
-  const MyOwnButton({super.key, required this.onTap, required this.text});
-  final VoidCallback onTap;
-  final String text;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: onTap,
-      height: 50,
-      minWidth: 200,
-      color: Colors.blue,
-      elevation: 10,
+class _MainPageState extends State<MainPage> {
+  final pages = [
+    const Center(
       child: Text(
-        text,
-        style: const TextStyle(
-          fontSize: 30,
+        'Inbox',
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Starred',
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Sent',
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Drafts',
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Trash',
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Span',
+      ),
+    ),
+  ];
+  VoidCallback updateState(int index) {
+    return () {
+      setState(() {
+        indexClicked = index;
+      });
+      Navigator.pop(context);
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(
           color: Colors.white,
         ),
+        title: const Text(
+          "Navigation Drawer",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: pages[indexClicked],
+      drawer: Drawer(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/drawer.jpg',
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+              padding: const EdgeInsets.all(0),
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const CircleAvatar(
+                    radius: 42,
+                    backgroundImage: AssetImage('assets/images/my-profile.jpg'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Augustine Ngobie',
+                    style: GoogleFonts.sanchez(
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'ngobiea@gmail.com',
+                    style: GoogleFonts.sanchez(
+                        fontSize: 10,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: [
+                  AppDrawerTile(
+                    index: 0,
+                    onTap: updateState(0),
+                  ),
+                  AppDrawerTile(
+                    index: 1,
+                    onTap: updateState(1),
+                  ),
+                  AppDrawerTile(
+                    index: 2,
+                    onTap: updateState(2),
+                  ),
+                  AppDrawerTile(
+                    index: 3,
+                    onTap: updateState(3),
+                  ),
+                  AppDrawerTile(
+                    index: 4,
+                    onTap: updateState(4),
+                  ),
+                  AppDrawerTile(
+                    index: 5,
+                    onTap: updateState(5),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const AppDrawerDivider(),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: Text(
+                      'JetMail',
+                      style: GoogleFonts.sanchez(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        fontStyle: FontStyle.italic,
+                        color: Defaults.drawerItemSelectedColor,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: Text(
+                      'Version 1.2.5',
+                      style: GoogleFonts.sanchez(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 12,
+                        fontStyle: FontStyle.italic,
+                        color: Defaults.drawerItemSelectedColor,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const AppDrawerDivider(),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
-class FooterWidget extends StatelessWidget {
-  const FooterWidget({super.key});
+class AppDrawerDivider extends StatelessWidget {
+  const AppDrawerDivider({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Icon(Icons.arrow_back),
-            Text('Developed by Ngobiea'),
-            Icon(Icons.arrow_forward)
-          ],
-        )
-      ],
+    return const Divider(
+      height: 1,
+      thickness: 2,
+      color: Defaults.drawerItemColor,
+      indent: 3,
+      endIndent: 3,
+    );
+  }
+}
+
+class AppDrawerTile extends StatelessWidget {
+  const AppDrawerTile({super.key, required this.index, required this.onTap});
+  final int index;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: ListTile(
+        selected: index == indexClicked,
+        selectedTileColor: Defaults.drawerSelectedTileColor,
+        onTap: onTap,
+        leading: Icon(
+          Defaults.drawerItemIcon[index],
+          size: 15,
+          color: indexClicked == index
+              ? Defaults.drawerItemSelectedColor
+              : Defaults.drawerItemColor,
+        ),
+        title: Text(
+          Defaults.drawerItemText[index],
+          style: GoogleFonts.sanchez(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+            color: indexClicked == index
+                ? Defaults.drawerItemSelectedColor
+                : Defaults.drawerItemColor,
+          ),
+        ),
+      ),
     );
   }
 }
